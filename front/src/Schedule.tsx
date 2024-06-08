f55eeabe35f7acbc41ea61be27c66bf0f5aebf95
ultramegapig <luvu4death@gmail.com>
2024-06-08 18:43:47 +0600

    da yebal ya eto

---
+++ front/src/Schedule.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/table.scss'
+
+const weekdays = [
+  
+]
+
+const Schedule: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+
+export default Schedule;
