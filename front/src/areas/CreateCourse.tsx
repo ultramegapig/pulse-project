b0af22cd9a167238801eff76692595dd7169d304
unknown <muntheoy>
2024-06-10 15:38:22 +0600

    create course maket

--- front/src/areas/CreateCourse.tsx
+++ front/src/areas/CreateCourse.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Select, { components, MultiValue } from 'react-select';
+import React, { useState } from "react";
+import Select, { components, MultiValue } from "react-select";
 
-// Определите интерфейс для опций
+
 interface Option {
   value: string;
   label: string;
@@ -9,15 +9,21 @@ interface Option {
 
 // Пример списка опций для выбора
 const options: Option[] = [
-  { value: 'МПБ-201-О-01', label: 'МПБ-201-О-01' },
-  { value: 'МПБ-202-О-02', label: 'МПБ-202-О-02' },
-  // Добавьте другие варианты здесь
+  { value: "МПБ-201-О-01", label: "МПБ-201-О-01" },
+  { value: "МПБ-202-О-02", label: "МПБ-202-О-02" },
+  
 ];
 
-// Кастомный компонент для MultiValueLabel (рамка для выбранных значений)
+
 const MultiValueLabel = (props: any) => (
   <components.MultiValueLabel {...props}>
-    <div style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '2px 5px' }}>
+    <div
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "4px",
+        padding: "2px 5px",
+      }}
+    >
       {props.data.label}
     </div>
   </components.MultiValueLabel>
@@ -27,33 +33,105 @@ const CreateCourse: React.FC = () => {
   const [selectedOptions, setSelectedOptions] = useState<Option[]>([]);
 
   const handleChange = (selected: MultiValue<Option>) => {
-    // Мы должны сохранить в состоянии массив объектов типа Option, а не только значения
-    setSelectedOptions(selected.map(option => ({ value: option.value, label: option.label })));
+    
+    setSelectedOptions(
+      selected.map((option) => ({ value: option.value, label: option.label }))
+    );
   };
 
+
+  const [courseName, setCourseName] = useState('');
+  const handleChangeCourseName = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setCourseName(event.target.value);
+};
+
+const [description, setDescription] = useState('');
+  const handleDescription = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setDescription(event.target.value);
+};
+
+const [syllabus, setSyllabus] = useState('');
+  const handleSyllabus = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSyllabus(event.target.value);
+};
+
+const [volume, setVolume] = useState('');
+  const handleVolume = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setVolume(event.target.value);
+};
+
   return (
-    <Select
-      isMulti
-      value={selectedOptions}
-      onChange={handleChange}
-      options={options}
-      components={{ MultiValueLabel }}
-      styles={{
-        control: (provided) => ({
-          ...provided,
-          borderColor: '#ccccff',
-          borderRadius: '12px',
-          boxShadow: 'none',
-          '&:hover': {
-            borderColor: '#aaaaee',
-          },
-        }),
-        multiValue: (styles) => ({
-          ...styles,
-          backgroundColor: '#f0f0ff',
-        }),
-      }}
-    />
+    <div>
+      <h1>Создайте новый курс</h1>
+      <div className="createLecture-topic">
+        <label htmlFor="textInput">Название курса</label>
+        <input
+          id="textInput"
+          type="text"
+          value={courseName}
+          onChange={handleChangeCourseName}
+          placeholder="Введите название курса"
+        />
+      </div>
+
+      <div className="createLecture-topic">
+        <label htmlFor="textInput">Описание</label>
+        <input
+          id="textInput"
+          type="text"
+          value={description}
+          onChange={handleDescription}
+          placeholder="Введите краткое описание курса"
+        />
+      </div>
+
+      <div className="createLecture-topic">
+        <label htmlFor="textInput">Ученый план</label>
+        <input
+          id="textInput"
+          type="text"
+          value={syllabus}
+          onChange={handleSyllabus}
+          placeholder="Вставьте ссылку на учебный план"
+        />
+      </div>
+
+      <div className="createLecture-topic">
+  <label htmlFor="textInput">Количество лекций</label>
+  <input
+    id="textInput"
+    type="number"
+    value={volume}
+    onChange={handleVolume}
+    placeholder="Вставьте количество лекций"
+    min="1"
+    step="1"
+  />
+</div>
+
+      <Select
+        isMulti
+        value={selectedOptions}
+        onChange={handleChange}
+        options={options}
+        components={{ MultiValueLabel }}
+        styles={{
+          control: (provided) => ({
+            ...provided,
+            borderColor: "#ccccff",
+            borderRadius: "12px",
+            boxShadow: "none",
+            "&:hover": {
+              borderColor: "#aaaaee",
+            },
+          }),
+          multiValue: (styles) => ({
+            ...styles,
+            backgroundColor: "#f0f0ff",
+          }),
+        }}
+      />
+    </div>
   );
 };
 
