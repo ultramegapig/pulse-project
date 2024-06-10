6006d462e0030b2e96c9fa03ba2f8446e7d1c10e
unknown <muntheoy>
2024-06-11 01:37:51 +0600

    test block in the tests page

---
+++ front/src/components/TestsBlock.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import TestShortCard from './TestShortCard';
+import '../styles/all.scss';
+
+interface TestCardData {
+    testName: string;
+    progressPercent: number;
+    deadColor: string;
+    deadline: string; 
+}
+
+interface TestsBlockProps {
+  testCardsData: TestCardData[];
+}
+
+const TestsBlock: React.FC<TestsBlockProps> = ({ testCardsData }) => {
+  return (
+    // <div className='testBlock'>
+    //   {testCardsData.map((cardData, index) => (
+    //     <TestShortCard
+    //           key={index}
+    //           testName={cardData.testName}
+    //           progressPercent={cardData.progressPercent}
+    //           deadColor={cardData.deadColor}
+    //           deadline={cardData.deadline} // Передаем дедлайн
+    //           backgroundColor={''}      />
+    //   ))}
+    // </div>
+  );
+}
+
+export default TestsBlock;
