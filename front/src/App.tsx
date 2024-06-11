885ed1709f0a9adbc242e06a09ced1360574be01
Elizaveta Savchenko <savchenko.liza.666@gmail.com>
2024-06-12 02:26:00 +0600

    fix side bar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,19 @@
 import '../src/styles/all.scss';
 import './styles/fonts.css';
 import Calendar from './images/calendar.svg';
-import React, { lazy, Suspense, useContext, useEffect } from 'react';
+import HomeIcon from './images/homeIcon.svg';
+import LectureIcon from './images/lectureIcon.svg';
+import TestsIcon from './images/testsIcon.svg';
+import StatisticIcon from './images/statisticIcon.svg';
+import ActiveHomeIcon from './images/activeHomeIcon.svg';
+import ActiveCalendar from './images/activeCalendarIcon.svg';
+import ActiveLectureIcon from './images/activeCourseIcon.svg';
+import ActiveTestsIcon from './images/activeTestsIcon.svg';
+import ActiveStatisticIcon from './images/activeStatisticIcon.svg';
+import React, { useState, lazy, Suspense, useContext, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AuthContext, AuthProvider } from './context/AuthContext';
-import Login from './components/Login';   
-import { Tab } from 'react-bootstrap';
-import CourseTestsBlock from './components/CourseTestsBlock';
-import Register from './areas/Register';
+import './styles/sideBar.scss';
 
 const MainPage = lazy(() => import('./areas/MainPage'));
 const Table = lazy(() => import('./areas/Table'));
@@ -17,37 +23,29 @@ const Progress = lazy(() => import('./areas/Progress'));
 const Numbers = lazy(() => import('./areas/Numbers'));
 const Podrobnosti = lazy(() => import('./areas/Podrobnosti'));
 
-const modules: { [key: number]: { path: string; component: React.LazyExoticComponent<React.ComponentType<any>>; label: string } } = {
-  1: { path: '/', component: MainPage, label: 'главное' },
-  2: { path: '/table', component: Table, label: 'расписание' },
-  3: { path: '/courses', component: Courses, label: 'курсы' },
-  4: { path: '/tests', component: Tests, label: 'тесты' },
-  5: { path: '/progress', component: Progress, label: 'успеваемость' }
-};
+interface Module {
+  path: string;
+  component: React.LazyExoticComponent<React.ComponentType<any>>;
+  label: string;
+  icon: string;
+  activeIcon: string;
+}
 
-const testCardsData = [
-  {
-    testName: 'Как делать куни болде',
-    progressPercent: 0, // Не начатый тест
-    deadColor: 'white',
-    deadline: '2024-12-31T23:59:59' // Дедлайн
-  },
-  {
-    testName: 'Как варить борщ',
-    progressPercent: 75,
-    deadColor: '#0000ff',
-    deadline: '2024-07-15T18:00:00' // Дедлайн
-  },
-  {
-    testName: 'Как изучать React',
-    progressPercent: 90,
-    deadColor: '#00ff00',
-    deadline: '2024-08-01T12:00:00' // Дедлайн
-  }
-];
+const modules: { [key: number]: Module } = {
+  1: { path: '/', component: MainPage, label: 'Главная', icon: HomeIcon, activeIcon: ActiveHomeIcon },
+  2: { path: '/table', component: Table, label: 'Расписание', icon: Calendar, activeIcon: ActiveCalendar },
+  3: { path: '/courses', component: Courses, label: 'Курсы', icon: LectureIcon, activeIcon: ActiveLectureIcon },
+  4: { path: '/tests', component: Tests, label: 'Тесты', icon: TestsIcon, activeIcon: ActiveTestsIcon },
+  5: { path: '/progress', component: Progress, label: 'Успеваемость', icon: StatisticIcon, activeIcon: ActiveStatisticIcon }
+};
 
 const AppContent: React.FC = () => {
   const { authState, setAuthState } = useContext(AuthContext);
+  const [activeLink, setActiveLink] = useState<number | null>(() => {
+    // Retrieve the active link from local storage or default to null
+    const storedActiveLink = localStorage.getItem('activeLink');
+    return storedActiveLink ? parseInt(storedActiveLink) : null;
+  });
 
   useEffect(() => {
     const token = localStorage.getItem('accessToken');
@@ -61,32 +59,37 @@ const AppContent: React.FC = () => {
     }
   }, [setAuthState]);
 
-  if (!authState.isAuthenticated) {
-    return <Login />;
-  }
+  const handleLinkClick = (key: number) => {
+    setActiveLink(key);
+    // Store the active link in local storage
+    localStorage.setItem('activeLink', key.toString());
+  };
 
   return (
     <div className="App">
       <header className="header">
-        <div className="logo">
-          logo
-        </div>
+        <div className="logo">logo</div>
         <div className="rightHeader">
-          <div className="notifications">
-            notification
-          </div>
-          <div className="userStuff">
-            user
-          </div>
+          <div className="notifications">notification</div>
+          <div className="userStuff">user</div>
         </div>
       </header>
 
       <div className="mainArea">
         <div className="sideBar">
-          {Object.keys(modules).map((key) => (
-            <Link key={key} to={modules[parseInt(key)].path} className="sideBarElement">
-              <img src={Calendar} alt="" />
-              <div>{modules[parseInt(key)].label}</div>
+        {Object.keys(modules).map((key) => (
+            <Link
+              key={key}
+              onClick={() => handleLinkClick(parseInt(key))}
+              to={modules[parseInt(key)].path}
+              className={`sideBarElement ${activeLink === parseInt(key) ? 'activeshit' : ''}`}
+            >
+              <img
+                className={`sidebar-icon ${activeLink === parseInt(key) ? 'activeshit' : ''}`}
+                src={activeLink === parseInt(key) ? modules[parseInt(key)].activeIcon : modules[parseInt(key)].icon}
+                alt=""
+              />
+              <div className='sideBar-text'>{modules[parseInt(key)].label}</div>
             </Link>
           ))}
         </div>
@@ -107,11 +110,12 @@ const AppContent: React.FC = () => {
   );
 }
 
+
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <Router>
-        <Register/>
+        <AppContent />
       </Router>
     </AuthProvider>
   );
